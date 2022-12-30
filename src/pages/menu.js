abb521b61be1bbc51a6686dca5ab663a1794ef42
allineo <alline.oliveira@gmail.com>
2022-12-30 17:20:32 -0300

    ''

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -8,7 +8,9 @@ import BebidasPage from './menupages/bebidas';
 
 function MenuPage(props) {
 
-  if (props.currentMenu === 'Pizzas') {
+  if (props.currentMenu === 'Combos') {
+    return (<CombosPage />);
+  } else if (props.currentMenu === 'Pizzas') {
     return (<PizzasPage />);
   } else if (props.currentMenu === 'Marmitas') {
     return (<MarmitasPage />);
@@ -19,8 +21,86 @@ function MenuPage(props) {
   } else if (props.currentMenu === 'Bebidas') {
     return (<BebidasPage />);
   } else {
-    return (<CombosPage />);
+    return (<MainMenu />);
   }
+}
+
+function MainMenu() {
+  return (
+    <div>
+      <MenuCombos />
+      <MenuPizzas />
+      <MenuMarmitas />
+      <MenuSucos />
+      <MenuCafe />
+      <MenuBebidas />
+    </div>);
+}
+
+function MenuCombos(props) {
+  return (
+    <div>
+      <img src='photos/bebidas/cocacola.jpg' alt="Coca-Cola" width="100" onClick={() => props.setCurrentMenu('Pizzas')} />&nbsp;&nbsp;
+      <img src='photos/bebidas/vinhos.jpg' alt="Vinhos" width="100" onClick={() => props.setCurrentMenu('Marmitas')} />&nbsp;&nbsp;
+      <img src='photos/bebidas/cocacola.jpg' alt="Sucos" width="100" onClick={() => props.setCurrentMenu('Sucos')} />&nbsp;&nbsp;
+      <img src='photos/bebidas/cocacola.jpg' alt="Café da Manhã" width="100" onClick={() => props.setCurrentMenu('Café')} />&nbsp;&nbsp;
+      <img src='photos/bebidas/cocacola.jpg' alt="Bebidas" width="100" onClick={() => props.setCurrentMenu('Bebidas')} />&nbsp;&nbsp;
+    </div>);
+}
+
+function MenuPizzas(props) {
+  return (
+    <div>
+      <img src='photos/bebidas/cocacola.jpg' alt="Coca-Cola" width="100" onClick={() => props.setCurrentMenu('Pizzas')} />&nbsp;&nbsp;
+      <img src='photos/bebidas/vinhos.jpg' alt="Vinhos" width="100" onClick={() => props.setCurrentMenu('Marmitas')} />&nbsp;&nbsp;
+      <img src='photos/bebidas/cocacola.jpg' alt="Sucos" width="100" onClick={() => props.setCurrentMenu('Sucos')} />&nbsp;&nbsp;
+      <img src='photos/bebidas/cocacola.jpg' alt="Café da Manhã" width="100" onClick={() => props.setCurrentMenu('Café')} />&nbsp;&nbsp;
+      <img src='photos/bebidas/cocacola.jpg' alt="Bebidas" width="100" onClick={() => props.setCurrentMenu('Bebidas')} />&nbsp;&nbsp;
+    </div>);
+}
+
+function MenuMarmitas(props) {
+  return (
+    <div>
+      <img src='photos/marmitas/marmitafit.jpg' alt="marmitafit" width="100" onClick={() => props.setCurrentMenu('Pizzas')} />&nbsp;&nbsp;
+      <img src='photos/marmitas/Feijoada.jpg' alt="Feijoada" width="100" onClick={() => props.setCurrentMenu('Marmitas')} />&nbsp;&nbsp;
+      <img src='photos/marmitas/Lasanha.jpg' alt="Lasanha" width="100" onClick={() => props.setCurrentMenu('Sucos')} />&nbsp;&nbsp;
+      <img src='photos/marmitas/Feijoada.jpg' alt="Café da Manhã" width="100" onClick={() => props.setCurrentMenu('Café')} />&nbsp;&nbsp;
+      <img src='photos/marmitas/marmitafit.jpg' alt="Bebidas" width="100" onClick={() => props.setCurrentMenu('Bebidas')} />&nbsp;&nbsp;
+    </div>);
+}
+
+function MenuSucos(props) {
+  return (
+    <div>
+      <img src='photos/sucos/sucoles.jpg' alt="Sucolé" width="100" onClick={() => props.setCurrentMenu('Pizzas')} />&nbsp;&nbsp;
+      <img src='photos/sucos/sucoles2.jpg' alt="Sucolé" width="100" onClick={() => props.setCurrentMenu('Marmitas')} />&nbsp;&nbsp;
+      <img src='photos/sucos/sucoles.jpg' alt="Sucolé" width="100" onClick={() => props.setCurrentMenu('Sucos')} />&nbsp;&nbsp;
+      <img src='photos/sucos/sucoles2.jpg' alt="Suco" width="100" onClick={() => props.setCurrentMenu('Café')} />&nbsp;&nbsp;
+      <img src='photos/sucos/sucoles.jpg' alt="Sucolé" width="100" onClick={() => props.setCurrentMenu('Bebidas')} />&nbsp;&nbsp;
+    </div>);
+}
+
+function MenuCafe(props) {
+  return (
+    <div>
+      <img src='photos/bebidas/cocacola.jpg' alt="Coca-Cola" width="100" onClick={() => props.setCurrentMenu('Pizzas')} />&nbsp;&nbsp;
+      <img src='photos/bebidas/vinhos.jpg' alt="Vinhos" width="100" onClick={() => props.setCurrentMenu('Marmitas')} />&nbsp;&nbsp;
+      <img src='photos/bebidas/cocacola.jpg' alt="Sucos" width="100" onClick={() => props.setCurrentMenu('Sucos')} />&nbsp;&nbsp;
+      <img src='photos/bebidas/cocacola.jpg' alt="Café da Manhã" width="100" onClick={() => props.setCurrentMenu('Café')} />&nbsp;&nbsp;
+      <img src='photos/bebidas/cocacola.jpg' alt="Bebidas" width="100" onClick={() => props.setCurrentMenu('Bebidas')} />&nbsp;&nbsp;
+    </div>);
+}
+
+function MenuBebidas(props) {
+  return (
+    <div>
+      <img src='photos/bebidas/cocacola.jpg' alt="Coca-Cola" width="100" onClick={() => props.setCurrentMenu('Pizzas')} />&nbsp;&nbsp;
+      <img src='photos/bebidas/vinhos.jpg' alt="Vinhos" width="100" onClick={() => props.setCurrentMenu('Marmitas')} />&nbsp;&nbsp;
+      <img src='photos/bebidas/cocacola.jpg' alt="Sucos" width="100" onClick={() => props.setCurrentMenu('Sucos')} />&nbsp;&nbsp;
+      <img src='photos/bebidas/vinhos.jpg' alt="Café da Manhã" width="100" onClick={() => props.setCurrentMenu('Café')} />&nbsp;&nbsp;
+      <img src='photos/bebidas/vinhos.jpg' alt="Bebidas" width="100" onClick={() => props.setCurrentMenu('Bebidas')} />&nbsp;&nbsp;
+    </div>);
 }
 
 export default MenuPage;
--- src/pages/menupages/bebidas.js
+++ src/pages/menupages/bebidas.js
@@ -4,23 +4,31 @@ function BebidasPage() {
       <table>
         <tr>
           <td>
-            <img src='photos/Pizzas.jpg' alt="Pizzas" width="150" />
+            <img src='photos/bebidas/cocacola.jpg' alt="Coca-Cola" width="150" />
           </td>
           <td>
-            <img src='photos/Marmita Fitness.jpg' alt="Marmita" width="150" />
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <img src='photos/Sucolés.jpg' alt="Sucolés" width="150" />
-          </td>
-          <td>
-            <img src='photos/Café.jpg' alt="Café" width="150" />
+            <img src='photos/Marmita Fitness.jpg' alt="Guaraná" width="150" />
           </td>
         </tr>
         <tr>
           <td>
-            <img src='photos/Lasanha.jpg' alt="Lasanha" width="150" />
+            <img src='photos/Sucolés.jpg' alt="Água com Gás" width="150" />
+          </td>
+          <td>
+            <img src='photos/Café.jpg' alt="Água sem Gás" width="150" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <img src='photos/Sucolés.jpg' alt="Heineken" width="150" />
+          </td>
+          <td>
+            <img src='photos/Café.jpg' alt="Brahma" width="150" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <img src='photos/bebidas/vinhos.jpg' alt="Vinhos" width="150" />
           </td>
           <td>
           </td>
