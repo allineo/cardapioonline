10ed05af950e1cc49d495a1f2e52e50ab39b7a06
allineo <alline.oliveira@gmail.com>
2023-03-09 21:42:33 -0300

    ''

--- src/pages/cardapiojson.js
+++ src/pages/cardapiojson.js
@@ -4,27 +4,29 @@ export const cardapio = {
     { "image": "frangocatupiry", "nome": "Pizza de Frango/Catupiry", "valor": 24.90, "unidade": "30cm - 6 fatias", "descricao": "Frango, Catupiry, Mussarela" },
     { "image": "calabresa", "nome": "Pizza de Calabresa", "valor": 24.90, "unidade": "30cm - 6 fatias", "descricao": "Calabresa, Mussarela" },
     { "image": "quatroqueijos", "nome": "Pizza Quatro Queijos", "valor": 24.90, "unidade": "30cm - 6 fatias", "descricao": "Catupiry, Cheddar, Prato, Mussarela" },
-    { "image": "mussarela", "nome": "Pizza Mozzarella", "valor": 19.90, "unidade": "30cm - 6 fatias", "descricao": "Mussarela" },
+    { "image": "mussarela", "nome": "Pizza Mozzarella", "valor": 24.90, "unidade": "30cm - 6 fatias", "descricao": "Mussarela" },
     { "image": "chocolate", "nome": "Fatias de Pizza de Chocolate", "valor": 6.90, "unidade": "1 fatia", "descricao": "Chocolate Preto ou Branco" }
   ],
 
   "marmitas": [
-    { "image": "lasanha", "nome": "Lasanha a Bolonhesa", "valor": 21.90, "unidade": "400g", "descricao": "Mussarela, Patinho moído, Massa, Presunto, Orégano" },
-    { "image": "feijoada", "nome": "Feijoada", "valor": 21.90, "unidade": "400g", "descricao": "Feijão Preto, Calabresa, Carne Seca, Couve, Arroz Branco" },
-    { "image": "carneseca", "nome": "Escondidinho de Carne Seca", "valor": 21.90, "unidade": "400g", "descricao": "Carne Seca desfiada, Aipim, Queijo, Molho Branco" },
-    { "image": "marmitafitfrango", "nome": "Marmita Gourmet Fitness de Frango", "valor": 21.90, "unidade": "400g", "descricao": "Arroz Vermelho, Peito de Frango ao Molho de Tomate Fresco e Açafrâo da Terra, Legumes ao vapor" },
-    { "image": "quichefrango", "nome": "Torta de Frango/Empadão", "valor": 21.90, "unidade": "400g", "descricao": "Massa de Empadão, Recheio de Frango, Cobertura de Queijo" }
+    { "image": "lasanha", "nome": "Lasanha a Bolonhesa", "valor": 23.90, "unidade": "400g", "descricao": "Mussarela, Patinho moído, Massa, Presunto, Orégano" },
+    { "image": "feijoada", "nome": "Feijoada", "valor": 23.90, "unidade": "400g", "descricao": "Feijão Preto, Calabresa, Carne Seca, Couve, Arroz Branco" },
+    { "image": "carneseca", "nome": "Escondidinho de Carne Seca", "valor": 23.90, "unidade": "400g", "descricao": "Carne Seca desfiada, Aipim, Queijo, Molho Branco" },
+    { "image": "marmitafitfrango", "nome": "Marmita Gourmet Fitness de Frango", "valor": 23.90, "unidade": "400g", "descricao": "Arroz Vermelho, Peito de Frango ao Molho de Tomate Fresco e Açafrâo da Terra, Legumes ao vapor" },
+    { "image": "quichecamarao", "nome": "Torta de Camarão (tipo Empadão)", "valor": 23.90, "unidade": "400g", "descricao": "Massa de Empadão, Recheio de Camarão, Cobertura de Queijo" },
+    { "image": "panquecavegana", "nome": "Panqueca de Alho Poró", "valor": 23.90, "unidade": "400g", "descricao": "Massa de Panqueca, Molho Branco de Alho Poró e Cenoura, Molho de Tomate Fresco e Açafrâo da Terra, Queijo" },
     /* { "image": "marmitafitcarne", "nome": "Marmita Gourmet Fitness de Carne", "valor": 19.90, "unidade": "400g", "descricao": "Arroz Basmati com Lentilhas, Picadinho de Carne ao Molho Madeira, Legumes ao vapor" }*/
   ],
 
   "sucos": [
     { "image": "sucouva", "nome": "Suco de Uva Integral", "valor": 8.00, "unidade": "taça - 300 ml", "descricao": "Uva Roxa Macerada (sem água, sem açúcar)" },
     { "image": "sucomanga", "nome": "Suco de Manga Integral", "valor": 8.00, "unidade": "taça - 300 ml", "descricao": "Manga Macerada (sem água, sem açúcar)" },
-    { "image": "sucolecoco", "nome": "Sucolé de Côco", "valor": 5.00, "unidade": "saquinho - 100 ml", "descricao": "Leite extraído do côco, Mel" },
+    { "image": "sorvetecoco", "nome": "Sorvete de Côco com Ganache", "valor": 5.00, "unidade": "200 ml", "descricao": "Sorvete de Côco seco, Calda de Chocolate Ganache" },
+    { "image": "sucolecoco", "nome": "Sucolé de Côco", "valor": 5.00, "unidade": "saquinho - 100 ml", "descricao": "Leite extraído do côco, Leite de Castanha de Cajú, Mel" },
     { "image": "sucoleacai", "nome": "Sucolé de Açaí", "valor": 5.00, "unidade": "saquinho - 100 ml", "descricao": "Açaí, Mel, Banana" },
     { "image": "sucoledetox", "nome": "Sucolé Detox", "valor": 5.00, "unidade": "saquinho - 100 ml", "descricao": "Gengibre, Limão, Couve, Uva verde Macerada" },
     { "image": "sucolemanga", "nome": "Sucolé de Manga", "valor": 5.00, "unidade": "saquinho - 100 ml", "descricao": "Manga Integral Macerada" },
-    { "image": "extratotomate", "nome": "Extrato de Tomate Italiano Integral", "valor": 5.00, "unidade": "saquinho - 150 ml", "descricao": "Tomate Italiano Integral Macerado" },
+    /*{ "image": "extratotomate", "nome": "Extrato de Tomate Italiano Integral", "valor": 5.00, "unidade": "saquinho - 150 ml", "descricao": "Tomate Italiano Integral Macerado" },*/
     { "image": "sucolemorango", "nome": "Sucolé de Morango", "valor": 5.00, "unidade": "saquinho - 100 ml", "descricao": "Morango Integral Macerado, Mel" },
     { "image": "sucoleuva", "nome": "Sucolé de Uva", "valor": 5.00, "unidade": "saquinho - 100 ml", "descricao": "Suco de Uva Roxa Integral Macerado" }
   ],
@@ -38,8 +40,8 @@ export const cardapio = {
   ],
 
   "bebidas": [
-    { "image": "heineken", "nome": "Cerveja Heineken", "valor": 10.00, "unidade": "latão - 470 ml", "descricao": "" },
-    { "image": "brahma", "nome": "Cerveja Brahma", "valor": 9.00, "unidade": "latão - 470 ml", "descricao": "" },
+    { "image": "heineken", "nome": "Cerveja Heineken", "valor": 11.00, "unidade": "latão - 470 ml", "descricao": "" },
+    { "image": "brahma", "nome": "Cerveja Brahma", "valor": 10.00, "unidade": "latão - 470 ml", "descricao": "" },
     { "image": "vinhotinto", "nome": "Vinho Tinto ou Branco", "valor": 55.00, "unidade": "garrafa", "descricao": "Cabernet Sauvignon, Chile, Concha Y Toro" },
     { "image": "coca", "nome": "Coca-Cola", "valor": 6.00, "unidade": "lata - 300 ml", "descricao": "Coca-Cola normal (com açúcar)" },
     { "image": "cocadiet", "nome": "Coca-Cola Sem Açúcar", "valor": 6.00, "unidade": "lata - 300 ml", "descricao": "Coca-Cola diet (SEM açúcar)" },
